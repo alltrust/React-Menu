7ad77ccfafc485a24541d31270c0ce2e4b3e6e22
Aldo <aldorgarcia29@gmail.com>
2022-04-01 11:12:52 -0400

    initial header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import './App.css';
+import styles from './App.module.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className={styles.App}>
+      <Header/>
     </div>
   );
+}
+
+const Header = ()=>{
+  return(
+    <div className={styles.mainHeader}>
+      <h1 >
+        React Meals
+      </h1>
+      <Button className={`${styles.button} ${styles.buttonLarge}`} type="button">CART</Button>
+
+    </div>
+  )
+}
+
+const Button = ({type, children, className})=>{
+  return(
+    <>
+    <button type={type} className={className}>
+      {children}
+    </button>
+    </>
+    
+  )
 }
 
 export default App;

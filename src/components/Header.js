b5fc3bcc18c2de35b23e05e922a830b1403ea7db
Aldo <aldorgarcia29@gmail.com>
2022-04-11 15:58:52 -0400

    animation with useEffect

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,50 @@
-import React,{useState, useContext} from 'react';
-import styles from './Header.module.css'
-import Button from '../IU/Button';
-import ModalWithOverlay from './ModalWithOverlay';
-import SelectedMealItemsContext from '../store/selectedMeals-context';
+import React, { useState, useContext, useEffect } from "react";
+import styles from "./Header.module.css";
+import Button from "../IU/Button";
+import ModalWithOverlay from "./ModalWithOverlay";
+import SelectedMealItemsContext from "../store/selectedMeals-context";
 
 const Header = () => {
-    const [showModal, setShowModal] = useState(false);
-    const selectedMealCtx= useContext(SelectedMealItemsContext)
+  const [showModal, setShowModal] = useState(false);
+  const [btnJump, setbtnJump] = useState(false);
+  const selectedMealCtx = useContext(SelectedMealItemsContext);
 
+  const numOfMealsInCart = selectedMealCtx.meals.reduce((current, item) => {
+    return current + item.amount;
+  }, 0);
 
-    const numOfMealsInCart = selectedMealCtx.meals.reduce((current,item)=>{
-        return current + item.amount
-    }, 0)
+  useEffect(() => {
+      setbtnJump(true);
+    const timer = setTimeout(() => {
+      setbtnJump(false);
+    }, 300);
+    return()=>{
+        clearTimeout(timer)
+    }
+  }, [selectedMealCtx.meals]);
 
-    console.log(numOfMealsInCart)
-  
-    const showModalHandler = () => {
-      setShowModal(true);
-    };
-    const closeModalHandler = () => {
-      setShowModal(false);
-    };
-    console.log("header");
-    return (
-      <div className={styles.mainHeader}>
-        <h1>React Meals</h1>
-        <Button
-          className={`${styles.button} ${styles.buttonLarge}`}
-          type="button"
-          onClick={showModalHandler}
-        >
-          CART <span>{numOfMealsInCart}</span>
-        </Button>
-        {showModal && <ModalWithOverlay closeModal={closeModalHandler} />}
-      </div>
-    );
+  console.log(numOfMealsInCart);
+
+  const showModalHandler = () => {
+    setShowModal(true);
   };
+  const closeModalHandler = () => {
+    setShowModal(false);
+  };
+  console.log("header");
+  return (
+    <div className={styles.mainHeader}>
+      <h1>React Meals</h1>
+      <Button
+        className={`${styles.button} ${styles.buttonLarge} ${btnJump ? styles.jump: ''}`}
+        type="button"
+        onClick={showModalHandler}
+      >
+        CART <span>{numOfMealsInCart}</span>
+      </Button>
+      {showModal && <ModalWithOverlay closeModal={closeModalHandler} />}
+    </div>
+  );
+};
 
-  export default Header
+export default Header;
